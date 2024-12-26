fc7823921d033905e1a3e4d1a8cb7f9b9a35b9fe
Multazam Arroihan Gusdiansyah <third2014project@gmail.com>
2024-12-26 18:23:47 +0700

    feat: created navigation util class and the extension

--- onboarding/onboarding_presentation/src/main/java/com/plcoding/onboarding_presentation/welcome/WelcomeScreen.kt
+++ onboarding/onboarding_presentation/src/main/java/com/plcoding/onboarding_presentation/welcome/WelcomeScreen.kt
@@ -15,10 +15,14 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import com.plcoding.core.R
+import com.plcoding.core.navigation.Route
+import com.plcoding.core.util.UiEvent
 import com.plcoding.onboarding_presentation.components.ActionButton
 
 @Composable
-fun WelcomeScreen() {
+fun WelcomeScreen(
+    onNavigate: (UiEvent.Navigate) -> Unit
+) {
     val spacing = LocalSpacing.current
 
     Column(
@@ -36,7 +40,7 @@ fun WelcomeScreen() {
         Spacer(modifier = Modifier.height(spacing.spaceMedium))
         ActionButton(
             text = stringResource(id = R.string.next),
-            onClick = {},
+            onClick = { onNavigate(UiEvent.Navigate(Route.AGE)) },
             modifier = Modifier.align(Alignment.CenterHorizontally)
         )
     }
